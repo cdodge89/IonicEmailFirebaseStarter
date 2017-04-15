b22df6cdd1e5ef7f2a335ac1b3ab60b735a42b89
Connor Dodge <connor.m.dodge@gmail.com>
2017-04-15 08:42:58 -0600

    added observer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,32 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { AngularFire } from 'angularfire2';
+import { Login } from '../pages/login/login'
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, af: AngularFire) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      const authObserver = af.auth.subscribe( user => {
+        if (user) {
+          this.rootPage = HomePage;
+          authObserver.unsubscribe();
+        } else {
+          this.rootPage = Login;
+          authObserver.unsubscribe();
+        }
+      });
     });
   }
+
 }
 
--- src/providers/auth-data.ts
+++ src/providers/auth-data.ts
@@ -2,17 +2,35 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the AuthData provider.
+import { AngularFire } from 'angularfire2';
+import firebase from 'firebase';
 
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class AuthData {
+  fireAuth: any;
+  constructor(public http: Http, public af: AngularFire) {
+    af.auth.subscribe( user => {
+        if (user) {
+            this.fireAuth = user.auth;
+            console.log(user);
+        }
+    });
+  }
 
-  constructor(public http: Http) {
-    console.log('Hello AuthData Provider');
+  loginUser(newEmail: string, newPassword: string): firebase.Promise<any> {
+      return this.af.auth.login({ email: newEmail, password: newPassword });
+  }
+
+  resetPassword(email: string): firebase.Promise<any> {
+      return firebase.auth().sendPasswordResetEmail(email);
+  }
+
+  logoutUser(): firebase.Promise<any> {
+      return this.af.auth.logout();
+  }
+  
+  signupUser(newEmail: string, newPassword: string): firebase.Promise<any> {
+      return this.af.auth.createUser({ email: newEmail, password: newPassword });
   }
 
 }
